6c98e5d1a8b1e5afa981da124085a73ac8976d02
qloo <exyloolq@gmail.com>
2019-04-11 08:52:20 +0800

    dianping css update

--- hello_spidery/utils/dianping_css_crack.py
+++ hello_spidery/utils/dianping_css_crack.py
@@ -22,19 +22,16 @@ from scrapy.selector import Selector
 class DianPingCssCracker:
     raw_html = attr.ib(default=None)
     shop_url = attr.ib(default='https://m.dianping.com/shop/23314835')
-    css_svg_mapping_dict = attr.ib(default=attr.Factory(dict))
+    cls_prefix_svg_mapping_dict = attr.ib(default=attr.Factory(dict))
     css_url_list = attr.ib(default=attr.Factory(list))
+    cls_prefix_px_mapping_dict = attr.ib(default=attr.Factory(dict))
+    cls_num_mapping_dict = attr.ib(default=attr.Factory(dict))
 
-    svg_url_match = re.compile(r'url\(//(.+?svgtextcss/.+?\.svg)')
+    svg_url_match = re.compile(r'span\[class\^="(\w+?)"\].+?url\((//.+?svgtextcss/.+?\.svg)')
+    class_attr_match = re.compile(r'\.(\w+){background:(.+?)px\s*(.+?)px;}')
 
     header = {
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-        'Accept-Encoding': 'gzip, deflate, br',
-        'Accept-Language': 'en,zh-CN;q=0.9,zh;q=0.8',
-        'Connection': 'keep-alive',
-        'Host': 'm.dianping.com',
-        'Upgrade-Insecure-Requests': '1',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/70.0.3538.102 Safari/537.36',
+        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) Chrome/73.0.3683.75 Safari/537.36'
     }
 
     def get_css_url(self):
@@ -47,8 +44,15 @@ class DianPingCssCracker:
     def get_svg_url(self, css_url):
         try:
             resp = requests.get(css_url, headers=self.header)
-            for svg_url in self.svg_url_match.findall(resp.text):
+            for cls, x, y in self.class_attr_match.findall(resp.text):
+                self.cls_prefix_px_mapping_dict.setdefault(cls[:3], []).append((cls, float(x), float(y)))
+
+            for cls_pixel_list in self.cls_prefix_px_mapping_dict.values():
+                cls_pixel_list.sort(key=lambda x: x[1])
+
+            for class_prefix, svg_url in self.svg_url_match.findall(resp.text):
                 svg_url = urljoin(self.shop_url, svg_url)
+                self.cls_prefix_svg_mapping_dict[class_prefix] = svg_url
                 yield svg_url
         except Exception:
             # self.logger.exception('fail to get svg url: ')
@@ -58,17 +62,35 @@ class DianPingCssCracker:
         resp = requests.get(svg_url, headers=self.header)
         return resp.text
 
-    def parse_svg_resp(self, html):
+    def parse_svg_resp(self, html, cls_pixel_list):
         font_list = re.findall(r'<text.*?y="(.*?)">(.*?)<', html)
+        if len(cls_pixel_list) == len(font_list[0][-1]):
+            for cls, number in zip(cls_pixel_list, font_list[0][-1][::-1]):
+                self.cls_num_mapping_dict[cls[0]] = number
+
+    def get_cls_prefix_by_svg_url(self, svg_url):
+        for cls_prefix, _svg_url in self.cls_prefix_svg_mapping_dict.items():
+            if _svg_url == svg_url:
+                return cls_prefix
 
     def crack(self, raw_html=None):
         if raw_html is not None:
             self.raw_html = raw_html
-        else:
+        elif raw_html is None and self.raw_html is None:
             raise ValueError('get wrong html')
         css_url = self.get_css_url()
         if css_url in self.css_url_list:
             return
 
         for svg_url in self.get_svg_url(css_url):
-            svg_resp = self.fetch_svg_resp(svg_url)
+            svg_html = self.fetch_svg_resp(svg_url)
+            cls_prefix = self.get_cls_prefix_by_svg_url(svg_url)
+            cls_pixel_list = self.cls_prefix_px_mapping_dict[cls_prefix]
+            self.parse_svg_resp(svg_html, cls_pixel_list)
+
+
+if __name__ == '__main__':
+    from html_dp import html
+
+    dp = DianPingCssCracker(raw_html=html)
+    dp.crack()
